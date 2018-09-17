5987e148756bede246f5f488dc0a08cf4b062e58
ParkChanjun(MNB) <dpldp92@daum.net>
2018-09-17 17:04:18 +0900

    스프라이트 추가 및 함수 뼈대 설정
    달리기 애니메이션을 위한 캐릭터 추가와 각 지점까지 도착을 위해 필요한 함수의 뼈대를 만들었다

--- Drills/Drill-05/Drill_05.py
+++ Drills/Drill-05/Drill_05.py
@@ -3,9 +3,28 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-#여기를 채워 넣으시오
+def goal_current_to_point(point_count):
+    point = ((203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 236),
+             (712, 349))
+    frame = 0
+    x, y = 800 // 2, 90
 
+def run_character():
+    derection = 1
+    point_count = 0
+
+    while point_count < 10:
+
+        goal_current_to_point(point_count)
+        point_count += 1
+
+
+
+
+while True:
+    run_character()
 
 close_canvas()
+
