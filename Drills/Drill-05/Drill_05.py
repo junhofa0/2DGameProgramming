31d76b9c93c065fb62959df69f05779c33da9bbb
ParkChanjun(MNB) <dpldp92@daum.net>
2018-09-17 17:23:24 +0900

    goal_current_to_point함수 내용 완성
    위의 함수에서 현재 위치에서 도착지점까지 캐릭터가 움직이는 방향을 바라본 채 달리는 함수 구현

--- Drills/Drill-05/Drill_05.py
+++ Drills/Drill-05/Drill_05.py
@@ -5,14 +5,42 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+
+
 def goal_current_to_point(point_count):
-    point = ((203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 236),
-             (712, 349))
+    point = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 236],
+             [712, 349]]
     frame = 0
-    x, y = 800 // 2, 90
+    direction = 1
+    global x
+    global y
+
+    while True:
+        clear_canvas_now()
+        grass.draw(400,90)
+
+        if point[point_count][0] - x > 0:
+            direction = 1
+        else:
+            direction = -1
+
+        x += (point[point_count][0] - x) / 20
+        y += (point[point_count][1] - y) / 20
+
+        if direction > 0:
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        elif direction < 0:
+            character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+
+        update_canvas()
+
+        frame = (frame + 1) % 8
+
+        delay(0.05)
+
+
 
 def run_character():
-    derection = 1
     point_count = 0
 
     while point_count < 10:
@@ -21,6 +49,7 @@ def run_character():
         point_count += 1
 
 
+x, y = 800 // 2, 90
 
 
 while True:
