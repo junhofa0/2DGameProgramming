785128950c7b559d3ca0b832c9eeb63f17aad884
ParkChanjun(MNB) <dpldp92@daum.net>
2018-09-20 16:55:51 +0900

    몇 가지 이벤트 구현
    마우스 움직일때마다 손모양의 이미지가 마우스 좌표를 따라 움직이는 이벤트의 내용과 ecs키나 x버튼을 눌렀을 때, 프로그램이 종료되는 것을 구현

--- Drills/Drill-06/move_character_with_mouse.py
+++ Drills/Drill-06/move_character_with_mouse.py
@@ -14,7 +14,13 @@ def handle_events():
     events = get_events()
     
     for event in events:
-        pass
+        if event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_QUIT:
+            running = False
             
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)  
