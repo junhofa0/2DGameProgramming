77570edfc00599799f16e1050a8e32420468afbc
ParkChanjun(MNB) <dpldp92@daum.net>
2018-10-04 17:06:41 +0900

    캐릭터의 곡선 루트 구현
    캐릭터의 이동 길인 곡선을 점 4개를 이용한 점과 점의 곡선길로 만듦

--- Drills/Drill-07/Drill8-2.py
+++ Drills/Drill-07/Drill8-2.py
@@ -9,7 +9,11 @@ grass = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 def Track_curve(i,p1,p2,p3,p4):
-    pass
+    t = i / 100
+        x =  ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y =  ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        return x, y
+    
 
 def goal_current_to_point(point_count):
     pass
