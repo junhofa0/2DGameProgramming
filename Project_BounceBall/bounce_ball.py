239e972ce4dddf738aff1b49112f8fde0264dc0e
ParkChanjun(MNB) <dpldp92@daum.net>
2018-10-15 21:54:25 +0900

    bounce_ball 초기화 함수
    각각 클래스에 대해서 속성 생성 및 속성값 초기화

--- Project_BounceBall/bounce_ball.py
+++ Project_BounceBall/bounce_ball.py
@@ -14,10 +14,22 @@ class block:
         pass
 
 class Grass:
-    pass
+    def __init__(self): 
+        self.x = 400
+        self.y = 30
+        self.image = load_image('grass.png')
 
 class Ball:
-    pass
+    def __init__(self): 
+        self.x = 400
+        self.y = 30
+        self.bump = False
+        self.acceleration = 3  # 가속도
+        self.speed = 30            # 속도
+        self.frame = 0
+        self.Direction = 0
+        self.image = load_image('resource\\image\\ball.png')   
+        self.image2 = load_image('resource\\image\\bump_ball.png')
 
 running = True
 grass = Grass()
