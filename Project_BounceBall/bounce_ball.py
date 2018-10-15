b5494bbc83d64e07e860712d15cfc6169a2154d8
ParkChanjun(MNB) <dpldp92@daum.net>
2018-10-15 21:51:51 +0900

    bounce_ball 함수구현
    최종 프로젝트 bounce_ball 클래스 정의 및 객체 생성

---
+++ Project_BounceBall/bounce_ball.py
@@ -0,0 +1,24 @@
+from pico2d import *
+
+Window_width, Window_high = 800, 600
+LEFT = -1
+RIGHT = 1
+JUMP_DOWN = 2
+JUMP_UP = 3
+STOP = 5
+
+open_canvas()
+
+class block:
+    def __init__(self):
+        pass
+
+class Grass:
+    pass
+
+class Ball:
+    pass
+
+running = True
+grass = Grass()
+ball = Ball()
