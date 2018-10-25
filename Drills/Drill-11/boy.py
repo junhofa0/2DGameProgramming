d331029e17f20e14465f821f84e2bd4148f22f7a
ParkChanjun(MNB) <dpldp92@daum.net>
2018-10-25 22:25:23 +0900

    상태 변환 벤다이어그램 추가
    dash 상태 추가로 그에 맞는 상태 변환 입력

--- Drills/Drill-11/boy.py
+++ Drills/Drill-11/boy.py
@@ -113,14 +113,19 @@ class SleepState:
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
                 RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
-                SLEEP_TIMER: SleepState, SPACE: IdleState
-                },
+                SLEEP_TIMER: SleepState, SPACE: IdleState,
+                DASH_DOWN: IdleState, DASH_UP: IdleState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
                LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
-               SPACE: RunState},
+               SPACE: RunState, DASH_DOWN: DashState,
+               DASH_UP: RunState},
     SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
                  LEFT_UP: RunState, RIGHT_UP: RunState,
-                 SPACE: IdleState}
+                 SPACE: IdleState, DASH_DOWN: IdleState,
+                 DASH_UP: IdleState}
+    DashState: {LEFT_UP: IdleState, RIGHT_UP: IdleState,
+                LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+                SPACE: DashState, DASH_UP: RunState}
 }
 
 
