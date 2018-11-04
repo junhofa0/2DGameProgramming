32994d6b4114862ef3e9fdef09d71e8d835003f3
junhofa0 <dpldp92@daum.net>
2018-11-04 18:35:31 +0900

    maptool 수정
    maptool을  framework에 맞춰서 수정

--- Project_BounceBall/maptool.py
+++ Project_BounceBall/maptool.py
@@ -1,39 +1,36 @@
-from pico2d import *
 import random
+import game_framework
+import lobby_state
 
-running = True
+from pico2d import *
 
-
-window_width, window_height = 1000, 600
-open_canvas(window_width, window_height)
 hide_cursor()
-
-
-ball_image = load_image("resource\\image\\ball.png")
-star_image = load_image("resource\\image\\star_sheet.png")
-block_thorn_image = load_image("resource\\image\\thorn_sheet.png")
-block_left_image = load_image("resource\\image\\leftboost.png")
-block_right_image = load_image("resource\\image\\rightboost.png")
-block_jump_image = load_image("resource\\image\\jump.png")
-block_basic_image = load_image("resource\\image\\block.png")
-block_broken_image = load_image("resource\\image\\block_broken.png")
-mouse_image = load_image("resource\\image\\mouse.png")
-mouse_click_image = load_image("resource\\image\\mouse_click.png")
-select_menu_image = load_image("resource\\image\\maptool_select.png")
-select_image = load_image("resource\\image\\select.png")
-portal_in_image = load_image("resource\\image\\portal_in.png")
-portal_out_image = load_image("resource\\image\\portal_out.png")
-mouse_down = False
+STOP = 6
+DIE = 7
+name = "MapTool"
+select_coor = {}
 select_x, select_y = None, None
+ball = None
+blocks = []
+star = None
+ball_image = None
+star_image = None
+block_thorn_image = None
+block_left_image = None
+block_right_image = None
+block_jump_image = None
+block_basic_image = None
+block_broken_image = None
+mouse_image = None
+mouse_click_image = None
+select_menu_image = None
+select_image = None
 mouse_x, mouse_y = None, None
 current_image = 0
 portal_blue = False
 portal_yellow = False
-
-select_coor = {1: (845, 477), 2: (901, 477), 3: (955, 477), 4: (845, 376), 5: (901, 376), 6: (955, 376),
-                   7: (845, 276), 8: (901, 276), 9: (955, 276), 0: (900, 70)}
-select_x, select_y = select_coor[0]
-
+mouse_down = False
+window_width, window_height = 1000, 600
 
 class Block:
     x = 0
@@ -84,11 +81,13 @@ class Ball:
         self.x = 0
         self.y = 0
         self.size = 30
+        self.state = STOP
 
     def draw(self):
         global ball_image
         if (self.x != None) and (self.y != None):
             ball_image.draw(self.x, self.y)
+
 
 class Star:
 
@@ -97,6 +96,7 @@ class Star:
         self.y = 0
         self.size = 40
         self.frame = 0
+        self.state = STOP
 
     def draw(self):
         global star_image
@@ -106,18 +106,142 @@ class Star:
     def update(self):
         self.frame = (self.frame + 1) % 40
 
+
+def enter():
+    global ball_image
+    global star_image
+    global block_thorn_image
+    global block_left_image
+    global block_right_image
+    global block_jump_image
+    global block_basic_image
+    global block_broken_image
+    global mouse_image
+    global mouse_click_image
+    global select_menu_image
+    global select_image
+    global portal_in_image
+    global portal_out_image
+    global select_coor
+    global select_x, select_y
+    global ball, blocks, star
+
+    ball_image = load_image("resource\\image\\ball.png")
+    star_image = load_image("resource\\image\\star_sheet.png")
+    block_thorn_image = load_image("resource\\image\\thorn_sheet.png")
+    block_left_image = load_image("resource\\image\\leftboost.png")
+    block_right_image = load_image("resource\\image\\rightboost.png")
+    block_jump_image = load_image("resource\\image\\jump.png")
+    block_basic_image = load_image("resource\\image\\block.png")
+    block_broken_image = load_image("resource\\image\\block_broken.png")
+    mouse_image = load_image("resource\\image\\mouse.png")
+    mouse_click_image = load_image("resource\\image\\mouse_click.png")
+    select_menu_image = load_image("resource\\image\\maptool_select4.png")
+    select_image = load_image("resource\\image\\select.png")
+    portal_in_image = load_image("resource\\image\\portal_in.png")
+    portal_out_image = load_image("resource\\image\\portal_out.png")
+
+    select_coor = {1: (845, 477), 2: (901, 477), 3: (955, 477), 4: (845, 376), 5: (901, 376), 6: (955, 376),
+                   7: (845, 276), 8: (901, 276), 9: (955, 276), 0: (900, 70)}
+    select_x, select_y = select_coor[0]
+    ball = Ball()
+    blocks = [Block() for n in range(300)]
+    star = Star()
+    count = 0
+    row, rank = 1, 1
+    for block in blocks:
+        block.x = (row - 1) * 40 + 20
+        block.y = (rank - 1) * 40 + 20
+        row += 1
+        count += 1
+        if count == 20:
+            row = 1
+            rank += 1
+            count = 0
+
+
+def exit():
+    global ball_image
+    global star_image
+    global block_thorn_image
+    global block_left_image
+    global block_right_image
+    global block_jump_image
+    global block_basic_image
+    global block_broken_image
+    global mouse_image
+    global mouse_click_image
+    global select_menu_image
+    global select_image
+    global portal_in_image
+    global portal_out_image
+    global select_coor
+    global ball, blocks, star
+
+    del ball_image
+    del star_image
+    del block_thorn_image
+    del block_left_image
+    del block_right_image
+    del block_jump_image
+    del block_basic_image
+    del block_broken_image
+    del mouse_image
+    del mouse_click_image
+    del select_menu_image
+    del select_image
+    del portal_in_image
+    del portal_out_image
+    del ball
+    del star
+
+    select_coor.clear()
+    blocks.clear()
+
+
+def update():
+    global blocks
+    global star
+
+    for block in blocks:
+        block.update()
+    star.update()
+
+
+def draw():
+    clear_canvas()
+
+    select_menu_image.draw(900, 300)
+    select_image.draw(select_x, select_y)
+    for block in blocks:
+        block.draw()
+    ball.draw()
+    Draw_mouse()
+    star.draw()
+
+    update_canvas()
+
+def pause(): pass
+
+
+def resume(): pass
+
+
 def handle_events():
     global running
     global mouse_x
     global mouse_y
     global mouse_down
-    global select_x
-    global select_y
-    global select_coor
     global current_image
+    global window_width, window_height
+    global blocks
+    global ball
+    global star
     global portal_blue
     global portal_yellow
-
+    global select_coor
+    global f
+    global select_x, select_y
     events = get_events()
 
     for event in events:
@@ -135,14 +259,14 @@ def handle_events():
                         blocks[(mouse_x // 40) + (mouse_y // 40) * 20].state = 0
                         portal_yellow = False
                     blocks[(mouse_x // 40) + (mouse_y // 40) * 20].state = current_image
+            else:
+                pass
 
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
-                running = False
-            elif (event.key >= SDLK_0) and (event.key <= SDLK_9):  # 48 <= event.key <= 57
-                current_image = event.key - 48
-                select_x, select_y = select_coor[current_image]
-            elif (event.key >= SDLK_0) and (event.key <= SDLK_9):  # 48 <= event.key <= 57
+                lobby_state.mouse_x, lobby_state.mouse_y = mouse_x, mouse_y
+                game_framework.change_state(lobby_state)
+            elif (event.key >= SDLK_0) and (event.key <= SDLK_9):   # 48 <= event.key <= 57
                 current_image = event.key - 48
                 select_x, select_y = select_coor[current_image]
             elif event.key == SDLK_s:
@@ -155,6 +279,7 @@ def handle_events():
                 mouse_down = True
                 mouse_x, mouse_y = event.x, window_height - 1 - event.y
                 if mouse_x <= 799:
+
                     if (current_image >= 0) and (current_image <= 6):
                         if blocks[(mouse_x // 40) + (mouse_y // 40) * 20].state == 7:
                             blocks[(mouse_x // 40) + (mouse_y // 40) * 20].state = 0
@@ -185,13 +310,14 @@ def handle_events():
 
                     elif current_image == 9:
                         ball.x, ball.y = mouse_x, mouse_y
-
+                else:
+                    pass
             elif event.button == SDL_BUTTON_RIGHT:
                 pass
-
         elif event.type == SDL_MOUSEBUTTONUP:
             if event.button == SDL_BUTTON_LEFT:
                 mouse_down = False
+
 
 def Draw_mouse():
     global mouse_x, mouse_y
@@ -203,6 +329,7 @@ def Draw_mouse():
         mouse_click_image.draw(mouse_x + 18, mouse_y - 6, 40, 40)
     else:
         mouse_image.draw(mouse_x + 18, mouse_y - 6, 40, 40)
+
 
 def save_file():
     global ball
@@ -223,22 +350,23 @@ def save_file():
                 Fop.write(str(block.x) + '\n')
                 Fop.write(str(block.y) + '\n')
                 Fop.write(str(block.state) + '\n')
-
             Fop.close()
             print("파일 덮어쓰기가 완료 되었습니다!!\n")
         elif answer == 'n' or answer == 'N':
+            print("\n")
             pass
     else:
         Fop = open(fname, "w")
         Fop.write(str(ball.x) + '\n')
         Fop.write(str(ball.y) + '\n')
+        Fop.write(str(ball.state) + '\n')
         Fop.write(str(star.x) + '\n')
         Fop.write(str(star.y) + '\n')
+        Fop.write(str(star.state) + '\n')
         for block in blocks:
             Fop.write(str(block.x) + '\n')
             Fop.write(str(block.y) + '\n')
             Fop.write(str(block.state) + '\n')
-
         Fop.close()
         print("새로운 파일이 생성 되었습니다!!\n")
 
@@ -250,7 +378,6 @@ def load_file():
     fstr = input("파일 불러오기 - 불러올 파일 이름 입력 : ")
     fname = "map\\" + fstr + ".txt"
     print(fname)
-
     if os.path.exists(fname):
         inF = open(fname, "r")
 
@@ -271,43 +398,7 @@ def load_file():
             block.x = int(bx)
             block.y = int(by)
             block.state = int(bstate)
-
         inF.close()
         print("해당 파일을 불러 왔습니다..\n")
-
     else:
         print("해당 파일이 존재하지 않습니다.\n")
-
-ball = Ball()
-blocks = [Block() for n in range(300)]
-star = Star()
-count = 0
-row, rank = 1, 1
-for block in blocks:
-    block.x = (row - 1) * 40 + 20
-    block.y = (rank - 1) * 40 + 20
-    row += 1
-    count += 1
-    if count == 20:
-        row = 1
-        rank += 1
-        count = 0
-
-
-while running:
-    clear_canvas()
-    handle_events()
-
-    select_menu_image.draw(900, 300)
-    select_image.draw(select_x, select_y)
-    for block in blocks:
-        block.draw()
-    ball.draw()
-    Draw_mouse()
-    star.draw()
-
-    update_canvas()
-
-    delay(0.01)
-
-close_canvas()
