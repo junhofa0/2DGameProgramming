10164b53e2bb42c0ba12e3934413bab7c8ab42e2
junhofa0 <dpldp92@daum.net>
2018-11-29 18:15:40 +0900

    게임에서 적용될 시간단위 생성

--- Project_BounceBall/pause_state.py
+++ Project_BounceBall/pause_state.py
@@ -7,3 +7,8 @@ from pico2d import *
 name = "PauseState"
 
 pause_image = None
+
+# Ball Speed
+PIXEL_PER_METER = (10.0 / 0.1)  # 10 pixel 10 cm = 100pixel 1m
+RUN_SPEED_MPS = 1.3
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)  # 1초에 1.3 METER = 130 PIXEL
